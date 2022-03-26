9774be034ac7d888d5de7cbaa10f197394f025be
chem2330 <97656256+chem2330@users.noreply.github.com>
2022-03-26 16:30:37 +0900

    [Gold V] Title: 로봇 청소기, Time: 88 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())  # 행렬 크기(N * M)
+x, y, d = map(int, input().split())  # 로봇 청소기 위치(x, y), 방향
+arr = [list(map(int, input().split())) for _ in range(N)]  # 빈칸 = 0/ 벽 = 1
+direction = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+cnt = 0
+flag = 0
+while True:
+    if flag == 1:
+        break
+    if arr[x][y] == 0:
+        arr[x][y] = 2
+        cnt += 1
+    for i in range(4):
+        d = (d - 1) % 4
+        if arr[x + direction[d][0]][y + direction[d][1]] == 0:
+            x, y = x + direction[d][0], y + direction[d][1]
+            break
+    else:
+        if arr[x - direction[d][0]][y - direction[d][1]] != 1:
+            x, y = x - direction[d][0], y - direction[d][1]
+        else:
+            flag = 1
+            break
+
+print(cnt)
