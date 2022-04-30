7fda32ea9fc6bbb6659f4cdbae566b90c97a0b54
chem2330 <97656256+chem2330@users.noreply.github.com>
2022-04-30 18:18:56 +0900

    [Silver III] Title: 상근이의 여행, Time: 180 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -1,12 +1,9 @@
 import sys
-input = sys.stdin.readline
 
+input = sys.stdin.readline
 T = int(input())
 for tc in range(T):
     N, M = map(int, input().split())  # 국가의 수, 비행기 종류
-    # arr = [[] for _ in range(N)]
     for _ in range(M):
         s, e = map(int, input().split())
-        # arr[s - 1].append(e - 1)
-        # arr[e - 1].append(s - 1)
     print(N - 1)
