ae48bffe49d678a08e7c66a025e0cf4c480fb137
chem2330 <97656256+chem2330@users.noreply.github.com>
2022-05-13 07:24:20 +0900

    [Gold V] Title: 숨바꼭질 2, Time: 308 ms, Memory: 34760 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,18 +1,20 @@
 from collections import deque
 
 
-
 S, E = map(int, input().split())  # 수빈이 위치, 동생 위치
 visited = [0] * 100001
 
 queue = deque([S])
 cnt = 0
 while queue:
-    v = queue.popleft()
+    v = queue.popleft()  # 꼭 popleft로 해야함, 방문 횟수를 하나씩 올려가면서 진행해야하기 때문(즉 dfs로만 가능)
+    if visited[E] != 0 and visited[v] > visited[E]:
+        break
     if v == E:
         cnt += 1
     else:
         for nv in [v + 1, v * 2, v - 1]:
+            # dfs이기 때문에 visited[nv] < visited[v] + 1의 경우는 나오지 않음
             if 0 <= nv <= 100000 and (visited[nv] == 0 or visited[nv] == visited[v] + 1):
                 visited[nv] = visited[v] + 1
                 queue.append(nv)
