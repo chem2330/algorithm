d9cc935f27e93c163a8e0a363617a852253bd99c
chem2330 <97656256+chem2330@users.noreply.github.com>
2022-05-14 22:35:22 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 304 ms, Memory: 46980 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,29 +1,34 @@
 import sys
-import heapq
-
 
 input = sys.stdin.readline
 
-V, E = map(int, input().split())
-visited = [0] * (V + 1)
-arr = [[] for _ in range(V + 1)]
-for _ in range(E):
-    s, e, w = map(int, input().split())
-    arr[s].append([w, e])
-    arr[e].append([w, s])
-# print(arr) = [[], [[1, 2], [3, 3]], [[1, 1], [2, 3]], [[2, 2], [3, 1]]]
 
-total = 0
+def find_set(x):
+    while x != v[x]:
+        x = v[x]
+    return x
+
+
+V, E = map(int, input().split())
+edge = []
+for _ in range(E):
+    v1, v2, w = map(int, input().split())
+    edge.append((w, v1, v2))
+edge.sort()  # 가중치 작은 것부터 오름차순정렬
+
 cnt = 0
-heap = [[0, 1]]
-while heap:
-    if cnt == V:
-        break
-    w, s = heapq.heappop(heap)
-    if not visited[s]:
-        visited[s] = 1
-        total += w
+total = 0
+v = list(range(V + 1))  # 정점들
+for w, v1, v2 in edge:
+    v1_root = find_set(v1)
+    v2_root = find_set(v2)
+    if v1_root != v2_root:
+        if v1_root > v2_root:
+            v[v1_root] = v2_root
+        else:
+            v[v2_root] = v1_root
         cnt += 1
-        for i in arr[s]:
-            heapq.heappush(heap, i)
+        total += w
+        if cnt == V - 1:
+            break
 print(total)
