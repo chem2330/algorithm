ef8c6cb908ac13c1d551eb47699f09e3ccd9bfc5
chem2330 <97656256+chem2330@users.noreply.github.com>
2022-07-10 20:50:22 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 372 ms, Memory: 48548 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,24 @@
+import sys
+
+
+input = sys.stdin.readline
+
+N = int(input())  # 노드의 수
+arr = [[] for _ in range(N + 1)]
+
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+P = [0] * (N + 1)
+
+stack = [1]
+while stack:
+    v = stack.pop()
+    for i in arr[v]:
+        if P[i] == 0:
+            P[i] = v
+            stack.append(i)
+for i in P[2:]:
+    print(i)
