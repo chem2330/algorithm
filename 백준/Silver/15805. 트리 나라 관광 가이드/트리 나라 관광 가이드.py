f2b1b8d6a2981b02cfb00222c18e665f1aa1fe09
chem2330 <97656256+chem2330@users.noreply.github.com>
2022-07-21 01:01:49 +0900

    [Silver I] Title: 트리 나라 관광 가이드, Time: 204 ms, Memory: 52336 KB -BaekjoonHub

--- 백준/Silver/15805. 트리 나라 관광 가이드/트리 나라 관광 가이드.py
+++ 백준/Silver/15805. 트리 나라 관광 가이드/트리 나라 관광 가이드.py
@@ -2,8 +2,8 @@ N = int(input())
 arr = list(map(int, input().split()))
 K = max(arr) + 1  # 총 노드 수
 print(K)
-P = [-2] * K
-P[arr[0]] = -1
+
+P = [-1] * K  # 각 노드의 부모 리스트 (루트 빼고 다 갱신 -1 넣어둠)
 visited = [0] * K
 visited[arr[0]] = 1
 
