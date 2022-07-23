6666fb8bdbc38801acfc1d0155d951519a99e30f
chem2330 <97656256+chem2330@users.noreply.github.com>
2022-07-24 00:47:16 +0900

    [Gold V] Title: 두 용액, Time: 168 ms, Memory: 42312 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,25 @@
+import sys
+
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+# print(arr) = [-99, -2, -1, 4, 98]
+
+solution = 10000000000
+start = 0
+end = N - 1
+ans = [0, 0]
+
+while start < end:
+    if abs(arr[start] + arr[end]) < solution:
+        solution = abs(arr[start] + arr[end])
+        ans = [arr[start], arr[end]]
+    if arr[start] + arr[end] < 0:
+        start += 1
+    else:
+        end -= 1
+
+print(*ans)
