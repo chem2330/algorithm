d768a210d8f8ac046ff1d9489af5421e951791ef
chem2330 <97656256+chem2330@users.noreply.github.com>
2022-12-16 19:05:08 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,21 +1,29 @@
-N, M = map(int, input().split())  # 행렬 크기(N * M)
-x, y, d = map(int, input().split())  # 로봇 청소기 위치(x, y), 방향
-arr = [list(map(int, input().split())) for _ in range(N)]  # 빈칸 = 0/ 벽 = 1
-direction = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-cnt = 0
+N, M = map(int, input().split())  # 세로, 가로
+r, c, d = map(int, input().split())  # 로봇 청소기 위치, 방향
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+answer = 0
 while True:
-    if arr[x][y] == 0:
-        arr[x][y] = 2
-        cnt += 1
-    for i in range(4):
-        d = (d - 1) % 4
-        if arr[x + direction[d][0]][y + direction[d][1]] == 0:
-            x, y = x + direction[d][0], y + direction[d][1]
+    # 1번
+    if arr[r][c] == 0:
+        answer += 1
+        arr[r][c] = -1
+    # 2번
+    for _ in range(4):
+        if d == 0:
+            d = 3
+        else:
+            d -= 1
+        nx, ny = r + direction[d][0], c + direction[d][1]
+        if arr[nx][ny] == 0:
+            r, c = nx, ny
             break
     else:
-        if arr[x - direction[d][0]][y - direction[d][1]] != 1:
-            x, y = x - direction[d][0], y - direction[d][1]
+        nx, ny = r - direction[d][0], c - direction[d][1]
+        if arr[nx][ny] != 1:
+            r, c = nx, ny
         else:
             break
-
-print(cnt)
+print(answer)
