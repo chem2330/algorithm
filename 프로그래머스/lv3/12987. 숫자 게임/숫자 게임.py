bb60c7cdc732971c0c8a01dd1d3de5d9aedb2d1c
chem2330 <97656256+chem2330@users.noreply.github.com>
2023-04-09 03:20:21 +0900

    [level 3] Title: 숫자 게임, Time: 38.47 ms, Memory: 18.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -0,0 +1,10 @@
+def solution(A, B):
+    answer = 0
+    A.sort(reverse=True)
+    B.sort(reverse=True)
+    idx = 0
+    for a in A:
+        if a < B[idx]:
+            answer += 1
+            idx += 1
+    return answer
