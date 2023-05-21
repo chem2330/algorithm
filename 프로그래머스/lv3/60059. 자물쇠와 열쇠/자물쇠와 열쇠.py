dd675c1c5291bab927fe739058bd7cadb63c845b
chem2330 <97656256+chem2330@users.noreply.github.com>
2023-05-21 21:15:27 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 5.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -1,40 +1,30 @@
-def spin_right(arr):
-    return list(map(list, zip(*arr[::-1])))
-
-def spin_left(arr):
-    return list(map(list, zip(*arr))[::-1])
-
-def check(arr, N):
-    answer = True
-    for ix in range(N):
-        for iy in range(N):
-            if arr[ix + N][iy + N] != 1:
-                return False
-    return answer
-
 def solution(key, lock):
     M = len(key)
     N = len(lock)
-    # 3배 더 큰 자물쇠 생성
-    new_lock = [[1] * N * 3 for _ in range(N * 3)]
-    # 기존 자물쇠를 새로운 자물쇠 가운데에 위치
-    for ix in range(N):
-        for iy in range(N):
-            new_lock[ix + N][iy + N] = lock[ix][iy]
-    # 시계방향으로 4번 돌림
+    
+    LOCK = [[0] * 60 for _ in range(60)]
+    for i in range(N):
+        for j in range(N):
+            LOCK[i + N][j + N] = lock[i][j]
+        
     for _ in range(4):
-        key = spin_right(key)
-        for lock_ix in range(N * 2):
-            for lock_iy in range(N * 2):
-                # key를 new_lock에 꽂음
-                for key_ix in range(M):
-                    for key_iy in range(M):
-                        new_lock[lock_ix + key_ix][lock_iy + key_iy] += key[key_ix][key_iy]
-                # new_lock의 중앙만 확인
-                if check(new_lock, N):
+        for Li in range(N * 2):
+            for Lj in range(N * 2):
+                
+                for Ki in range(M):
+                    for Kj in range(M):
+                        LOCK[Li + Ki][Lj + Kj] += key[Ki][Kj]
+                
+                flag = 0
+                for i in range(N):
+                    for j in range(N):
+                        if LOCK[i + N][j + N] != 1:
+                            flag = 1
+                if not flag:
                     return True
-                # key를 new_lock에서 뺌
-                for key_ix in range(M):
-                    for key_iy in range(M):
-                        new_lock[lock_ix + key_ix][lock_iy + key_iy] -= key[key_ix][key_iy]
+                
+                for Ki in range(M):
+                    for Kj in range(M):
+                        LOCK[Li + Ki][Lj + Kj] -= key[Ki][Kj]
+        key = list(map(list, zip(*key[::-1])))
     return False
