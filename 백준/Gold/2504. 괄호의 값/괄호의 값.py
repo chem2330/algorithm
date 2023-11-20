586ff42a833052c7f7fcd63ac4f05169b77247fc
chem2330 <97656256+chem2330@users.noreply.github.com>
2023-11-20 20:34:28 +0900

    [Gold V] Title: 괄호의 값, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,46 @@
+
+
+char = list(input())
+q = []
+cal = []
+flag = 0
+
+for c in char:
+    if c == '(':
+        q.append(2)
+    elif c == '[':
+        q.append(3)
+    elif c == ')':
+        if q and q[-1] == 2:
+            cal.append([len(q), q[-1]])
+            q.pop()
+        else:
+            print(0)
+            flag = 1
+            break
+    elif c == ']':
+        if q and q[-1] == 3:
+            cal.append([len(q), q[-1]])
+            q.pop()
+        else:
+            print(0)
+            flag = 1
+            break
+
+if flag == 0:
+    if q:
+        print(0)
+    else:
+        ans = [0 for _ in range(100)]
+        ans[cal[0][0]] = cal[0][1]
+    
+        for i in range(1, len(cal)):
+            a1, b1 = cal[i - 1][0], cal[i - 1][1]
+            a2, b2 = cal[i][0], cal[i][1]
+            if a1 <= a2:
+                ans[a2] += b2
+            elif a1 > a2:
+                ans[a2] += ans[a1] * b2
+                ans[a1] = 0
+    
+        print(ans[1])
