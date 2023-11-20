e342c245f0e791f3107c6033e98a9eab6d9844de
chem2330 <97656256+chem2330@users.noreply.github.com>
2023-11-20 20:35:34 +0900

    [Gold V] Title: 괄호의 값, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -1,5 +1,3 @@
-
-
 char = list(input())
 q = []
 cal = []
@@ -15,7 +13,6 @@ for c in char:
             cal.append([len(q), q[-1]])
             q.pop()
         else:
-            print(0)
             flag = 1
             break
     elif c == ']':
@@ -23,24 +20,26 @@ for c in char:
             cal.append([len(q), q[-1]])
             q.pop()
         else:
-            print(0)
             flag = 1
             break
 
-if flag == 0:
-    if q:
-        print(0)
-    else:
-        ans = [0 for _ in range(100)]
-        ans[cal[0][0]] = cal[0][1]
-    
-        for i in range(1, len(cal)):
-            a1, b1 = cal[i - 1][0], cal[i - 1][1]
-            a2, b2 = cal[i][0], cal[i][1]
-            if a1 <= a2:
-                ans[a2] += b2
-            elif a1 > a2:
-                ans[a2] += ans[a1] * b2
-                ans[a1] = 0
-    
-        print(ans[1])
+if q:
+    flag = 1
+
+if flag == 1:
+    print(0)
+
+else:
+    ans = [0 for _ in range(16)]
+    ans[cal[0][0]] = cal[0][1]
+
+    for i in range(1, len(cal)):
+        a1, b1 = cal[i - 1][0], cal[i - 1][1]
+        a2, b2 = cal[i][0], cal[i][1]
+        if a1 <= a2:
+            ans[a2] += b2
+        elif a1 > a2:
+            ans[a2] += ans[a1] * b2
+            ans[a1] = 0
+
+    print(ans[1])
